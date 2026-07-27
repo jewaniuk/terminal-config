591d242737689a47a41673a62223364503210816
jewaniuk <jacob.ewaniuk@queensu.ca>
2026-07-26 22:59:29 -0400

    added a script to automatically install all dependencies and link configuration files from this repo

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# install homebrew if not installed
+if ! command -v brew &> /dev/null; then
+    echo "installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# install all Brewfile dependencies
+echo "installing dependencies..."
+brew bundle --file=~/.dotfiles/Brewfile
+
+# create ~/.config directory if it doesn't exist
+mkdir -p ~/.config
+
+# create symlinks for configurations
+echo "creating symlinks..."
+ln -sf ~/.dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/config/ghostty ~/.config/ghostty
+ln -sf ~/.dotfiles/config/gitui ~/.config/gitui
+ln -sf ~/.dotfiles/config/starship.toml ~/.config/starship.toml
+
+echo "✨ environment setup complete!"
